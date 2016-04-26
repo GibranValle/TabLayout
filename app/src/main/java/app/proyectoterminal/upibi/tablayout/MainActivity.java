a417e03695c595035f33960e3ac07108748ed215
GibranValle <gibranvalle@hotmail.com>
2016-04-26 03:30:39 -0500

    Funcionando

--- app/src/main/java/app/proyectoterminal/upibi/tablayout/MainActivity.java
+++ app/src/main/java/app/proyectoterminal/upibi/tablayout/MainActivity.java
@@ -1,6 +1,7 @@
 package app.proyectoterminal.upibi.tablayout;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
@@ -9,21 +10,27 @@ import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.ImageSpan;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
 {
     /***                                CONSTANTES                                       */
     public static final int TABS_NUM = 4;
+    /*
     private String[] titulos;
     private int[] icons ={R.drawable.ic_menu_manage,
             R.drawable.ic_menu_search,R.drawable.ic_menu_today,
             R.drawable.ic_menu_view};
+    */
 
     // ARRAY DE TITULOS PARA TAB
     private String TAG = "Interfaz";
@@ -35,9 +42,7 @@ public class MainActivity extends AppCompatActivity
 
 
     /********************************** VARIABLES **************************************/
-    private Toolbar toolbar;
-    private TabLayout tabLayout;
-    private ViewPager viewPager;
+
     /********************************** VARIABLES **************************************/
 
 
@@ -52,17 +57,24 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         // Get the ViewPager and set it's PagerAdapter so that it can display items
-        viewPager = (ViewPager) findViewById(R.id.pager);
-        viewPager.setAdapter(new SampleFragmentPagerAdapter(getSupportFragmentManager(),
-                MainActivity.this));
+        ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
+        SampleFragmentPagerAdapter pagerAdapter =
+                new SampleFragmentPagerAdapter(getSupportFragmentManager(), MainActivity.this);
+        viewPager.setAdapter(pagerAdapter);
 
         // Give the TabLayout the ViewPager
-        tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
         tabLayout.setupWithViewPager(viewPager);
+
+        // Iterate over all tabs and set the custom view
+        for (int i = 0; i < tabLayout.getTabCount(); i++) {
+            TabLayout.Tab tab = tabLayout.getTabAt(i);
+            tab.setCustomView(pagerAdapter.getTabView(i));
+        }
     }
     /********************************** ETAPAS **************************************/
 
@@ -102,13 +114,27 @@ public class MainActivity extends AppCompatActivity
     }
 
     public class SampleFragmentPagerAdapter extends FragmentPagerAdapter {
-        final int PAGE_COUNT = 3;
-        private String tabTitles[] = new String[] { "Tab1", "Tab2", "Tab3" };
+        final int PAGE_COUNT = 4;
+        String[] titulos = getResources().getStringArray(R.array.titulos);
+        int[] icons ={R.drawable.ic_menu_view,
+                R.drawable.ic_menu_today,R.drawable.ic_menu_search,
+                R.drawable.ic_menu_manage};
+
         private Context context;
 
         public SampleFragmentPagerAdapter(FragmentManager fm, Context context) {
             super(fm);
             this.context = context;
+        }
+
+        public View getTabView(int position) {
+            // Given you have a custom layout in `res/layout/custom_tab.xml` with a TextView and ImageView
+            View v = LayoutInflater.from(context).inflate(R.layout.custom_tab, null);
+            TextView tv = (TextView) v.findViewById(R.id.tab_text);
+            tv.setText(titulos[position]);
+            ImageView img = (ImageView) v.findViewById(R.id.tab_image);
+            img.setImageResource(icons[position]);
+            return v;
         }
 
         @Override
@@ -124,7 +150,13 @@ public class MainActivity extends AppCompatActivity
         @Override
         public CharSequence getPageTitle(int position) {
             // Generate title based on item position
-            return tabTitles[position];
+            Drawable image = context.getResources().getDrawable(icons[position]);
+            image.setBounds(0, 0, image.getIntrinsicWidth(), image.getIntrinsicHeight());
+            // Replace blank spaces with image icon
+            SpannableString sb = new SpannableString("   " + titulos[position]);
+            ImageSpan imageSpan = new ImageSpan(image, ImageSpan.ALIGN_BOTTOM);
+            sb.setSpan(imageSpan, 0, 1, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+            return sb;
         }
     }
 
